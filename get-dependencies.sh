69bfe5ccf257b75a85019f2e76b25d817d2adeac
juju4 <juju4@users.noreply.github.com>
2016-10-30 18:29:54 -0400

    missing golang role dep

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,5 +7,6 @@ rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 [ ! -d $rolesdir/juju4.mhn ] && git clone https://github.com/juju4/ansible-mhn $rolesdir/juju4.mhn
 
