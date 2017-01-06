3026787423f917bf57046193242b5f74fd33844f
juju4 <juju4@users.noreply.github.com>
2017-01-06 12:38:25 -0500

    sync repositories - add Jenkinsfile, galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,4 +9,10 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 [ ! -d $rolesdir/juju4.mhn ] && git clone https://github.com/juju4/ansible-mhn $rolesdir/juju4.mhn
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.mhnclient ] && ln -s ansible-mhnclient $rolesdir/juju4.mhnclient
+[ ! -e $rolesdir/juju4.mhnclient ] && cp -R $rolesdir/ansible-mhnclient $rolesdir/juju4.mhnclient
+
+## don't stop build on this script return code
+true
 
