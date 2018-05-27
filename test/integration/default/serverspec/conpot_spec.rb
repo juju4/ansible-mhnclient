7e06d89cd964fe58ab0ea46ca9caeb7fcbb5f3bb
juju4 <juju4@users.noreply.github.com>
2018-05-27 18:45:56 -0400

    conpot: add pycrypto requirements + serverspec tests

--- test/integration/default/serverspec/conpot_spec.rb
+++ test/integration/default/serverspec/conpot_spec.rb
@@ -8,6 +8,13 @@ describe file('/var/log/supervisor/conpot.log') do
   its(:content) { should_not match /Error/ }
 end
 
+describe file('/var/log/supervisor/conpot.out') do
+  its(:content) { should match /Starting Conpot using template/ }
+  its(:content) { should_not match /Error/ }
+  its(:content) { should_not match /ImportError: No module named/ }
+  its(:content) { should_not match /Address already in use/ }
+end
+
 describe command('mongo hpfeeds -eval "db.auth_key.find({identifier: \'mnemosyne\'}).pretty();"') do
   its(:stdout) { should match /conpot.events/ }
   its(:exit_status) { should eq 0 }
