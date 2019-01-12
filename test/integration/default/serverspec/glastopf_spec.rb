fc8c62ed0ac0f8192eca0f49b0370ee9cc665bdb
juju4 <juju4@users.noreply.github.com>
2019-01-12 11:12:11 +0000

    disable glastopf in test/default

--- test/integration/default/serverspec/glastopf_spec.rb
+++ test/integration/default/serverspec/glastopf_spec.rb
@@ -3,6 +3,7 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+glastopf_test = false
 geoloc_test = false
 
 ### FIXME! can't select right python process
@@ -21,23 +22,25 @@ geoloc_test = false
 #  its(:stdout) { should match /xxx/ }
 #end
 
-describe file('/var/log/supervisor/glastopf.out') do
-  its(:content) { should_not match /Error/ }
-  its(:content) { should_not match /ImportError: No module named/ }
-end
+if glastopf_test
+  describe file('/var/log/supervisor/glastopf.out') do
+    its(:content) { should_not match /Error/ }
+    its(:content) { should_not match /ImportError: No module named/ }
+  end
 
-describe port(80), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
-  it { should be_listening }
-end
+  describe port(80), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
+    it { should be_listening }
+  end
 
-describe command('mongo hpfeeds -eval "db.auth_key.find({identifier: \'mnemosyne\'}).pretty();"') do
-  its(:stdout) { should match /glastopf.events/ }
-  its(:exit_status) { should eq 0 }
-end
-
-if geoloc_test
-  describe command('mongo hpfeeds -eval "db.auth_key.find({identifier: \'geoloc\'}).pretty();"') do
+  describe command('mongo hpfeeds -eval "db.auth_key.find({identifier: \'mnemosyne\'}).pretty();"') do
     its(:stdout) { should match /glastopf.events/ }
     its(:exit_status) { should eq 0 }
+  end
+
+  if geoloc_test
+    describe command('mongo hpfeeds -eval "db.auth_key.find({identifier: \'geoloc\'}).pretty();"') do
+      its(:stdout) { should match /glastopf.events/ }
+      its(:exit_status) { should eq 0 }
+    end
   end
 end
