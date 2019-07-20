e502cba5962e0df95ef32c1b88b8fb29dfa90ae9
juju4 <juju4@users.noreply.github.com>
2019-07-20 07:02:55 -0400

    update galaxy dependencies role name

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 [ ! -d $rolesdir/juju4.mhn ] && git clone https://github.com/juju4/ansible-mhn $rolesdir/juju4.mhn
 ## galaxy naming: kitchen fails to transfer symlink folder
